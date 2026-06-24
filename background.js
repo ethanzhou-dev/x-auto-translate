d78a686adc0a1acfa2e8a036f1ee31018e15804e
Ethan Zhou <zhouyireitik@gmail.com>
2026-06-24 22:01:12 +0800

    update 5 files

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'translate') {
-    // 使用 MyMemory API 代替被拉黑的 Google API，增加一个通用邮箱以获取每天 50,000 词的免费额度
     const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(request.text)}&langpair=Autodetect|${request.targetLang}&de=x-auto-translate-user@example.com`
 
     fetch(url)
